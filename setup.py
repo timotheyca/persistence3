70b11525bef2cce0774ab75cd314b1f461f0ea9b
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-09-12 23:51:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='persistence3',
     version='0.0.1.1',
     packages=['ptvp3'],
-    url='',
-    license='',
-    author='Timofey',
-    author_email='',
+    url='https://github.com/timotheyca/persistence3',
+    license='BSD 3-Clause',
+    author='timotheyca',
+    author_email='tim@ongoteam.yaconnect.com',
     description=''
 )
