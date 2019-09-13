e3f5919b7d47b43841d6cb6452dc3a3e36b48079
timotheyca <12258572+timotheyca@users.noreply.github.com>
2019-09-13 20:54:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ptv-p3',
-    version='a0.0.1.1',
+    version='a0.0.2.1',
     packages=['ptvp3', 'ptvp3.abstraction'],
     url='https://github.com/timotheyca/persistence3',
     license='BSD 3-Clause',
